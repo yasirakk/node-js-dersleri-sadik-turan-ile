f63cde8af27d853e3d2afc679f8ba10fe81beb18
Yaser Akkurt <yasi.akurt@gmail.com>
2024-05-10 01:12:27 +0300

    Bootstrap yüklemeden önceki hali korumak için

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-var http = require('http'); // node modullerinden biri\ onun ile birlikte geldi
-var fs = require('fs'); // node modullerinden biri\ onun ile birlikte geldi/ dosya okuma ve yazma icin kullanilirstart
+const express = require('express');
+const app = express();
 
+app.set('view engine', 'ejs'); // view engine olarak ejs kullanacağız
+app.use(express.static('public')); // public klasöründeki dosyaları kullanima acmak icin
+app.use(express.static('node_modules')); 
 
+const urunTablosu = [
+    {
+        id: 1,
+        isim: 'Telefon',
+        fiyat: 1000,
+        isActive: true,
+        imageUrl: 'telefon-foto.jpg',
+    },
+    {
+        id: 2,
+        isim: 'Kamera',
+        fiyat: 2000,
+        isActive: true,
+        imageUrl: 'kamera-foto.jpg',
 
-var server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile('index.html', (err, data) => {
-            res.write(data)
-            res.end();
-        });
-
-    } else if(req.url === '/urunler') {
-        fs.readFile('urunler.html', (err, data) => {
-            res.write(data)
-            res.end();
-        });
-    } else{
-        fs.readFile('404.html', (err, data) => {
-            res.write(data)
-            res.end();
-        });    
+    },
+    {
+        id: 3,
+        isim: 'Klavye',
+        fiyat: 3000,
+        isActive: true,
+        imageUrl: 'klavye-foto.jpg',
     }
+];
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-server.listen(3000, () => {
-    console.log('node.js server at 3000 port');
+app.get('/urunler/:id', (req, res) => {
+    const urun = urunTablosu.find(item => item.id === parseInt(req.params.id));
+    res.render('urundetay', urun);
+});
+
+app.get('/urunler', (req, res) => {
+    res.render('urunler', { urunler: urunTablosu });
+});
+
+// 404 için en sona bir middleware ekleyin
+app.use((req, res) => {
+    res.render('404');
+});
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
